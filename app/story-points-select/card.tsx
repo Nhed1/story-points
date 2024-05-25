04b0d04d2408461304a593a59fea2e23f3bfb505
leonardo <leonardogarreto@gmail.com>
2024-05-25 16:17:49 -0300

    feat: change visibility behavior

--- app/story-points-select/card.tsx
+++ app/story-points-select/card.tsx
@@ -3,13 +3,20 @@
 interface ICard {
   selectedPoint: number;
   username: string;
+  visible: boolean;
 }
 
-export default function Card({ selectedPoint, username }: ICard) {
+export default function Card({ selectedPoint, username, visible }: ICard) {
   return (
     <div className="flex flex-col p-2 ">
       <div className={`w-2 p-5 flex justify-center bg-emerald-900`}>
-        <p className="font-semibold text-lg text-white">{selectedPoint}</p>
+        <p
+          className={`font-semibold text-lg ${
+            visible ? "text-white" : "text-transparent"
+          }`}
+        >
+          {selectedPoint}
+        </p>
       </div>
 
       <p className="">{username || "---"}</p>
--- app/story-points-select/reveal-cards.tsx
+++ app/story-points-select/reveal-cards.tsx
@@ -30,17 +30,21 @@ export default function RevealCards({
   return (
     <div className="flex flex-col justify-center items-center mb-6">
       <div className="flex flex-col gap-4 items-center">
-        {visible && (
-          <div className="flex">
-            {storyPoints.map((storyPoint, index) => (
-              <Card
-                key={index}
-                selectedPoint={storyPoint.selectedPoint}
-                username={storyPoint.user}
-              />
-            ))}
-          </div>
-        )}
+        <div className="flex">
+          {storyPoints.map((storyPoint, index) => {
+            if (storyPoint.selectedPoint) {
+              return (
+                <Card
+                  visible={visible}
+                  key={index}
+                  selectedPoint={storyPoint.selectedPoint}
+                  username={storyPoint.user}
+                />
+              );
+            }
+          })}
+        </div>
+
         <Button
           className="max-w-28"
           onClick={() => {
